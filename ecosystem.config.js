5e99e1a0b61b4da7fcfda54fa39e38d6e300152a
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2020-11-15 22:00:31 +0100

    run build:ci in ci

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
             repo: 'https://github.com/dennispidun/uni-hi-discord-bot',
             path: '/home/unibot',
             'post-deploy':
-            'npm i && npm run build && pm2 reload ecosystem.config.js --env production && pm2 save',
+            'npm i && npm run build:ci && pm2 reload ecosystem.config.js --env production && pm2 save',
             env: {
             NODE_ENV: 'production',
             DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
