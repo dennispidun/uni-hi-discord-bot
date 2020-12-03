32c338c920c7a9845a768523eb47c39819735734
Dennis Pidun <dennis.pidun@uni-hildesheim.de>
2020-12-03 16:05:33 +0100

    fix env problem for pm2

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -28,7 +28,7 @@ module.exports = {
             repo: 'https://github.com/dennispidun/uni-hi-discord-bot',
             path: '/home/unibot',
             'post-deploy':
-            'pwd && npm i && npm run build:ci && pm2 reload /home/unibot/src/ecosystem.config.js --env production && pm2 save',
+            'pwd && npm i && npm run build:ci && pm2 restart /home/unibot/src/ecosystem.config.js --env production && pm2 save',
             env: {
             NODE_ENV: 'production',
             DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
